4e45391ff3520dfe5b53cdc05f41fe9505eee760
Kazu Takahashi <num96kazu19@icloud.com>
2022-02-02 20:07:47 +0900

    onClickとuseState

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState("Kazu");
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ["Taka", "Kazu", "Hiro"];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]);
+  };
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  const handleClick2 = () => {
+    console.log(count);
+  };
+
   return (
     <main>
-      <p>Hello World!!</p>
+      <p onDoubleClick={handleClick}>Hello {name}!!</p>
+      <button onClick={handleNameChange}>Change name</button>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={handleClick2}>Click it 2</button>
     </main>
   );
 };
